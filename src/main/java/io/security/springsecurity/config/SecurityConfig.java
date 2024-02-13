22380509e03a4587796655f194ee5fcb4e148635
bomingeem <bomingeem@gmail.com>
2024-02-13 22:08:00 +0900

    fix: Remember Me 인증필터: RememberMeAuthenticationFilter

--- src/main/java/io/security/springsecurity/config/SecurityConfig.java
+++ src/main/java/io/security/springsecurity/config/SecurityConfig.java
@@ -4,15 +4,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -24,6 +23,9 @@ import java.io.IOException;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -71,12 +73,7 @@ public class SecurityConfig {
                 .rememberMe((remember) -> remember
                         .rememberMeParameter("remember")
                         .tokenValiditySeconds(3600)
-                        .userDetailsService(new UserDetailsService() {
-                            @Override
-                            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                                return null;
-                            }
-                        }));
+                        .userDetailsService(userDetailsService));
         return http.build();
     }
 }
